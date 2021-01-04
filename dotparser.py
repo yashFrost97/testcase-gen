58fb6c8254a5f294ecd93dffbc139a03ce2e5fc6
Yash Patange <yash.frost1997@gmail.com>
2021-01-04 11:16:01 -0800

    Updated dotparser.py to run from cmd line!

--- dotparser.py
+++ dotparser.py
@@ -1,15 +1,26 @@
 """"
+Parser for the dot file dumped by TLA+ toolbox.
+Usage: python dotparser.py [absolute file path]
 a -> b[label="", foo=", bar=""] - This is an edge from a to b, [] contains attributes of the edge
-a[] - This denotes a node and its attributes
+a[] - This denotes a node and its attributes.
 """
 import collections
 import re
 from collections import deque
 import tlaparser
 from copy import deepcopy
-# color 2 = blue : START
-# color 3 = lightgreen : PHASE1
-# color 4 = darkgreen : PHASE2
+import argparse
+import sys
+
+
+# -8631748407493987020 [label="/\\ decision = <<FALSE, FALSE, FALSE>>\n/\\ k = <<1, 1, 1>>\n/\\ p1v = <<1, 0, 0>>\n/\\ pc = <<\"START\", \"START\", \"START\">>\n/\\ p2v = <<-1, -1, -1>>\n/\\ p2Msg = {}\n/\\ decided = <<-1, -1, -1>>\n/\\ p1Msg = {}",style = filled]
+node_pattern = re.compile(r'(?P<id>-?\d+)\s+\[label="(?P<tla_stuff>.+)".*];?')
+
+# 6798208728600954568 -> 8768434477171639267 [label="",color="2",fontcolor="2"];
+edge_pattern = re.compile(
+    r'(?P<from_node>-?\d+)\s+->\s+(?P<to_node>-?\d+)\s+'
+    r'\[label="(?P<label>.+?)"(,|).*];?')
+
 
 class DotGraph:
 
@@ -64,11 +75,9 @@ class DotGraph:
 				path.append(next)
 				backtrack(next, path)
 				path.pop()
-				
 
 		path = deque([source])
 		backtrack(source, path)
-		print(len(results))
 
 		return results
 
@@ -84,48 +93,65 @@ class DotGraph:
 		return graph
 
 
-# -8631748407493987020 [label="/\\ decision = <<FALSE, FALSE, FALSE>>\n/\\ k = <<1, 1, 1>>\n/\\ p1v = <<1, 0, 0>>\n/\\ pc = <<\"START\", \"START\", \"START\">>\n/\\ p2v = <<-1, -1, -1>>\n/\\ p2Msg = {}\n/\\ decided = <<-1, -1, -1>>\n/\\ p1Msg = {}",style = filled]
-node_pattern = re.compile(r'(?P<id>-?\d+)\s+\[label="(?P<tla_stuff>.+)".*];?')
+def dotparser(lines):
+	new_graph = DotGraph()
+	node_count = 0
+	edge_count = 0
+	print("Parsing Lines")
+	for line in lines:
+		# regex match for nodes
+		node_match = node_pattern.match(line)
+		if node_match:
+			node_count += 1
+			tla_stuff = node_match.group("tla_stuff")
+			id = node_match.group("id")
+			# parse the tla stuff to a state
+			# if first state set it as the starting state -> init state
+			# for the rest: add the state to the graph
+			tla = tla_stuff.encode().decode("unicode_escape")
+			state = tlaparser.parse_tla(tla)
+			if new_graph.init_state is None:
+				new_graph.init_state = state
+			else:
+				new_graph.add_state(state, id)
+			continue
+		# regex match for edges
+		edge_match = edge_pattern.match(line)
+		if edge_match:
+			edge_count += 1
+			from_id = edge_match.group("from_node")
+			to_id = edge_match.group("to_node")
+			tla_label = edge_match.group("label")
 
-# 6798208728600954568 -> 8768434477171639267 [label="",color="2",fontcolor="2"];
-edge_pattern = re.compile(
-    r'(?P<from_node>-?\d+)\s+->\s+(?P<to_node>-?\d+)\s+'
-    r'\[label="(?P<label>.+?)"(,|).*];?')
+			new_graph.add_edge(tla_label, int(from_id), int(to_id))
 
-file = open("D:\Programs\Python\TestCase\DOTFiles\Model_1_labels.dot", "r")
-lines = file.readlines()
-file.close()
+	print("Parsing Done")
+	print("Node count: ", node_count, " Edge count: ", edge_count)
+	print("Returning the graph")
+	return new_graph
 
-new_graph = DotGraph()
-node_count = 0
-edge_count = 0
-for line in lines:
-	# regex match for nodes
-	node_match = node_pattern.match(line)
-	if node_match:
-		node_count += 1
-		tla_stuff = node_match.group("tla_stuff")
-		id = node_match.group("id")
-		# parse the tla stuff to a state
-		# if first state set it as the starting state -> init state
-		# for the rest: add the state to the graph
-		tla = tla_stuff.encode().decode("unicode_escape")
-		state = tlaparser.parse_tla(tla)
-		if new_graph.init_state is None:
-			new_graph.init_state = state
-		else:
-			new_graph.add_state(state, id)
-		continue
-	# regex match for edges
-	edge_match = edge_pattern.match(line)
-	if edge_match:
-		edge_count += 1
-		from_id = edge_match.group("from_node")
-		to_id = edge_match.group("to_node")
-		tla_label = edge_match.group("label")
 
-		new_graph.add_edge(tla_label, int(from_id), int(to_id))
+def main():
+	my_args = argparse.ArgumentParser(description="Input the absolute .dot file path")
+	my_args.add_argument('Path', metavar="path", type=str, help="Absolute .dot File Path")
+	arg = my_args.parse_args()
+	input = arg.Path
 
-print("Node count: ", node_count, " Edge count: ", edge_count)
+	try:
+		file = open(input, "r")
+		lines = file.readlines()
+		file.close()
 
-traces = new_graph.all_traces()
+		print("File found!")
+		print("Beginning to parse the DOT File")
+		graph = dotparser(lines)
+
+		print("Generating all possible trace behaviours!")
+		traces = graph.all_traces() # contains all paths
+		print(f"Number of trace behaviours are: {len(traces)}")
+
+	except:
+		print(f'File: {input} does not exist!')
+
+if __name__ == "__main__":
+	main()
